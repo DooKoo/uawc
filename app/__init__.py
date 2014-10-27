733f8c20feb2d677e4f42f5d3392d99af11d4224
Sheikspir <sheikspirit@gmail.com>
2014-10-27 19:41:22 +0200

    another push / for illya

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
 __author__ = 'dookoo'
 from flask import Flask
 app = Flask(__name__)
+app.secret_key = "^xeb(xf87%3/x83_(x86x15x08x1cx16Jx9exd6x18x8exd7d"
+from app import views
--- app/views.py
+++ app/views.py
@@ -1,15 +1,19 @@
 from app import app
-from flask import request, render_template
+from flask import request, render_template, sessions, flash
 from app import models
 
 
 @app.route('/')
 def index():
-    return render_template('admin.html')
+    return render_template('shop.html')
 
 @app.route('/product')
 def product():
     return render_template('product.html')
+
+@app.error_handler(404)
+def page_not_found():
+    return render_template('page_not_found.html')
 
 @app.route('/basket')
 def basket():
@@ -25,5 +29,4 @@ def add():
 
 @app.route('/admin')
 def admin():
-
     return render_template('admin.html')

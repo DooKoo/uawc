acaeb4f7535fedf271e0beec977554cdd4303032
Sheikspir <sheikspirit@gmail.com>
2014-10-29 20:48:40 +0200

    jinja for all + small updates models

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 __author__ = 'dookoo'
 from flask import Flask
+import os
 app = Flask(__name__)
-app.secret_key = "^xeb(xf87%3/x83_(x86x15x08x1cx16Jx9exd6x18x8exd7d"
+app.secret_key = os.urandom(24)
 from app import views
--- app/views.py
+++ app/views.py
@@ -7,7 +7,9 @@ from werkzeug.exceptions import HTTPException, NotFound
 from werkzeug.wsgi import responder
 from werkzeug.wrappers import BaseRequest
 
-database = db.DBwork()
+DATABASE = db.DBwork()
+USERS_ON_SITE = 0
+CARTS = {}
 
 # APP_ROOT = os.path.join(os.path.abspath(__file__))
 UPLOADER_FOLDER = './app/static/images/products/'
@@ -31,6 +33,16 @@ def view(request):
     raise NotFound()
 
 
+def sign_in():
+    global USERS_ON_SITE
+    global CARTS
+    if 'id' not in session:
+        session['id'] = USERS_ON_SITE
+
+        USERS_ON_SITE += 1
+        CARTS = {session['id']: models.Cart()}
+        return redirect('/catalog')
+
 
 @app.errorhandler(404)
 def page_not_found(error):
@@ -51,18 +63,41 @@ def login():
 
 @app.route('/product=<int:product_id>', methods=['GET'])
 def product(product_id):
-    product_db = database.get_product(product_id)
+    sign_in()
+
+    global CARTS
+    cart = CARTS[session['id']]
+
+    product_db = DATABASE.get_product(product_id)
     product_db['views'] += 1
-    database.update_product(product_id, models.Product.from_json(product_db))
+    DATABASE.update_product(product_id, models.Product.from_json(product_db))
     return render_template('product.html',
                            name=product_db['name'],
                            address=product_db['photo'],
                            description=product_db['description'],
-                           price=product_db['price'])
+                           price=product_db['price'],
+                           number_of_items=cart.num_of_items,
+                           id=product_db['id'])
+
+
+@app.route('/logout')
+def logout():
+    global USERS_ON_SITE
+    global CARTS
+
+    USERS_ON_SITE -= 1
+
+    del CARTS[session['id']]
+    session.clear()
+
+    return redirect('/catalog')
 
 
 @app.route('/cart')
 def basket():
+
+    sign_in()
+    cart = CARTS
     return render_template('cart.html')
 
 
@@ -78,20 +113,30 @@ def add():
         image = request.files["add_to_shop_image"]
         image.save(os.path.join(app.config['UPLOAD_FOLDER'], image.filename))
 
-        database.add(models.Product(request.form["add_to_shop_name"], request.form["add_to_shop_about"],
+        DATABASE.add(models.Product(request.form["add_to_shop_name"], request.form["add_to_shop_about"],
                      request.form["add_to_shop_price"], app.config['UPLOAD_FOLDER'][5:]+image.filename))
 
     return redirect('/product=5')
 
 
+@app.route('/add_to_cart', methods=['POST'])
+def add_to_cart():
+    global CARTS
+    if request.method == 'POST':
+        product_id = request.form['id']
+        CARTS.get(session['id']).add(models.Product.from_json(DATABASE.get_product(product_id)))
+
+
 @app.route('/catalog')
 def catalog():
+    sign_in()
     return render_template('catalog.html')
 
 
 @app.route('/test')
 def test():
-    return render_template('outlay.html')
+    session.clear()
+    return "ok"
 
 
 @app.route('/')

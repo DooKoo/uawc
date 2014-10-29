afcfff6375ccef1c7d9870b21bedb7cfbc1ca358
Sheikspir <sheikspirit@gmail.com>
2014-10-29 14:40:02 +0200

    no header  + small upfates

--- app/views.py
+++ app/views.py
@@ -11,15 +11,18 @@ UPLOADER_FOLDER = './app/static/images/products/'
 app.config['UPLOAD_FOLDER'] = UPLOADER_FOLDER
 app.config['ALLOWED_EXTENSIONS'] = set(['png', 'jpg', 'jpeg', 'gif'])
 
+
 def validate(user, passw):
     if user == 'admin' and passw == '000':
         return True
     else:
         return False
 
+
 def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+
 
 @app.errorhandler(404)
 def page_not_found(error):
@@ -35,7 +38,8 @@ def login():
             session['username'] = username
             return redirect('/admin')
     else:
-        return
+        return redirect('/test')
+
 
 @app.route('/product=<int:product_id>', methods=['GET'])
 def product(product_id):
@@ -47,13 +51,16 @@ def product(product_id):
                            description=product_db['description'],
                            price=product_db['price'])
 
+
 @app.route('/basket')
 def basket():
     return render_template('basket.html')
 
+
 @app.route('/checkout')
 def checkout():
     return render_template('check.out.html')
+
 
 @app.route('/add', methods=['POST'])
 def add():
@@ -66,6 +73,7 @@ def add():
                      request.form["add_to_shop_price"], app.config['UPLOAD_FOLDER'][5:]+image.filename))
     return redirect('/product/0')
 
+
 @app.route('/test')
 def test():
     return render_template('outlay.html')
@@ -75,7 +83,7 @@ def test():
 @app.route('/admin')
 def admin():
     if 'username' in session:
-        return render_template('admin.html')
+        return render_template('admin.html', invisible_overwrite='')
     else:
-        return render_template('login.html')
+        return render_template('login.html', invisible_overwrite='')
 

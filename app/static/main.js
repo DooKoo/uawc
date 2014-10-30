77c4948a80d9d7e4ebcaecda00f19b85839fb4dd
Sheikspir <sheikspirit@gmail.com>
2014-10-30 21:07:29 +0200

    such a sad commit :'(

--- app/static/main.js
+++ app/static/main.js
@@ -1,10 +1,8 @@
 function add_to_cart(id){
-    $.post('/add_to_cart', {id: id, from:window.location.toString()} );
-document.getElementById("number_of_items").innerHTML=+innerHTML+1;
+    $.post('/add_to_cart', {id: id, from:window.location.toString()});
+    getElementById("number_of_items").innerHTML = + innerHTML + 1;
 };
-function proceed_to_checkout(user_id){
-    $.post('/checkout', {user_id: user_id};
-};
-function proceed_to_buy(user_id){
-    $.post('/buy', {user_id: user_id};
+
+function remove_from_cart(id){
+    $.post('/remove_from_cart', {id: id});
 };

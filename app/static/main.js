755f6c3ffebd40a1dd68b32d1baa4157c811b1ed
Sheikspir <sheikspirit@gmail.com>
2014-10-30 17:01:04 +0200

    some lil reworks

--- app/static/main.js
+++ app/static/main.js
@@ -1,3 +1,9 @@
 function add_to_cart(id){
-    $.post('/add_to_cart', {id: id, from:window.location.toString()});
+    $.post('/add_to_cart', {id: id, from:window.location.toString()} );
+}
+function proceed_to_checkout(user_id){
+    $.post('/checkout', {user_id: user_id}
+}
+function proceed_to_buy(user_id){
+    $.post('/buy', {user_id: user_id}
 }
